d4791212fbfb8b1027e412f21dc83e4b25766618
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-18 21:28:43 +0530

    Add files via upload

---
+++ stringCreate.py
@@ -0,0 +1,35 @@
+# String
+# Creating string in Python
+str1= 'Python'
+print(str1)
+
+# Accessing characters in Python String
+
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+print(str1[5])
+
+# String slicing
+print(str1[:])
+print(str1[0:])
+print(str1[:5])
+print(str1[:3])
+print(str1[0:2])
+
+print(str1[-1])
+print(str1[-2])
+print(str1[-3])
+print(str1[-4])
+print(str1[-5])
+
+print(str1[-3:-1])
+print(str1[-4:-1])
+print(str1[-5:-3])
+
+#Updating string
+str2 ='Hello, world!'
+print("Updating string: ", str2[:6]+'Python')
+
